e24c9379d38f6ffbeeba9f9e21d50654066ccd9d
Solessfir <solessfir@gmail.com>
2024-01-08 18:30:39 +0100

    Premake updated

--- premake5.lua
+++ premake5.lua
@@ -3,42 +3,52 @@ project "ImGui"
 	language "C++"
     staticruntime "off"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("../../Binaries/" .. outputdir .. "/%{prj.name}")
+	objdir ("../../Intermediate/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
-		"imconfig.h",
 		"imgui.h",
 		"imgui.cpp",
-		"imgui_draw.cpp",
 		"imgui_internal.h",
+		"imconfig.h",
+		"imgui_draw.cpp",
 		"imgui_tables.cpp",
 		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
+		"backends/imgui_impl_glfw.h",
+		"backends/imgui_impl_glfw.cpp",
+		"backends/imgui_impl_opengl3.h",
+		"backends/imgui_impl_opengl3.cpp",
+	}
+
+	includedirs
+	{
+		"../ImGui",
+		"../GLFW/include",
 	}
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
+		cppdialect "C++20"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
-		cppdialect "C++17"
+		cppdialect "C++20"
 
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
 
-	filter "configurations:Release"
+	filter "configurations:Development"
 		runtime "Release"
 		optimize "on"
 
-    filter "configurations:Dist"
+    filter "configurations:Shipping"
 		runtime "Release"
 		optimize "on"
         symbols "off"
